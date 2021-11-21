3c10ec37ab236170830d2bd835526497a950b18f
Bruno Silva Santana <ibrunoss@outlook.com>
2021-11-20 22:21:18 -0300

    Chore: Project structuring
    - Remove useless create-next-app files
    - Added Template Layout

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from "next";
+
+import TemplateLayout from "../components/Template/Layout";
+
+const Home: NextPage = () => {
+  return (
+    <TemplateLayout>
+      <h1>Home Page</h1>
+    </TemplateLayout>
+  );
+};
+
+export default Home;
